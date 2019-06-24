40136b6f0465866bf4a4e8a815ed2f4a19b5d77e
Nikolai Rozanov <nikolai.rozanov@gmail.com>
2019-06-24 01:29:51 +0100

    starting with simple bayesian estimators

---
+++ barl/agents/stateless_bayesian.py
@@ -0,0 +1,46 @@
+####################################################
+#
+# This Work is written by Nikolai Rozanov <nikolai>
+#
+# Contact:  nikolai.rozanov@gmail.com
+#
+# Copyright (C) 2018-Present Nikolai Rozanov
+#
+####################################################
+
+####################################################
+# IMPORT STATEMENTS
+####################################################
+
+# >>>>>>  Native Imports  <<<<<<<
+
+# >>>>>>  Package Imports <<<<<<<
+import numpy as np
+
+# >>>>>>  Local Imports   <<<<<<<
+from barl import estimators
+from barl.utils import utils
+
+from .interfaces import BaseStateLessAgent
+
+
+####################################################
+# CODE
+####################################################
+
+
+
+
+
+
+
+####################################################
+# MAIN
+####################################################
+
+
+
+
+
+
+# EOF
---
+++ barl/estimators/simple_bayesian.py
@@ -0,0 +1,39 @@
+####################################################
+#
+# This Work is written by Nikolai Rozanov <nikolai>
+#
+# Contact:  nikolai.rozanov@gmail.com
+#
+# Copyright (C) 2018-Present Nikolai Rozanov
+#
+####################################################
+
+####################################################
+# IMPORT STATEMENTS
+####################################################
+
+# >>>>>>  Native Imports  <<<<<<<
+
+# >>>>>>  Package Imports <<<<<<<
+import numpy as np
+
+# >>>>>>  Local Imports   <<<<<<<
+from barl.utils import numerical
+
+from .interfaces import BaseEstimator
+####################################################
+# CODE
+####################################################
+
+
+
+
+
+
+
+####################################################
+# MAIN
+####################################################
+
+
+# EOF
