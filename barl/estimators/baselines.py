Bug Fixes on utils.numerical (numpy.copy needs to be returned)

barl/estimators/baselines.py
@@ -37,6 +37,13 @@ class EmpiricalMean(BaseEstimator):
         self.__shape = self.__value.shape
 
 
+    def __str__(self):
+        """
+        Represents the empricial mean as a string
+        """
+        outStr = str(self.__estimate)
+        return outStr
+
     def update( self , newValueOrList, indexList=None):
         """
         updates the mean
@@ -46,9 +53,9 @@ class EmpiricalMean(BaseEstimator):
         newValue = self.__deal_with_valueList( newValueOrList, indexList )
         oneHotIndexList = self.__deal_with_index( indexList )
 
+
         if newValue.shape == self.__shape:
             self.__update_single_value(newValue, oneHotIndexList)
-
 
 
         elif (newValue.ndim == self.__value.ndim+1) and (newValue.shape[1]==self.__value.shape[0]):
@@ -66,18 +73,17 @@ class EmpiricalMean(BaseEstimator):
 
 
 
-
-
     def __update_single_value(self, newValue, oneHotIndexList=None):
         """
         updates a single value
         """
         if type(oneHotIndexList)==type(None):
-            where = True
+            where = np.ones(self.__shape)
         else:
             where = oneHotIndexList
 
-        self.__value = np.add( self.__value, newValue, where=where>0 )
+        # self.__value = np.add( self.__value, newValue, where=where>0 )
+        self.__value += newValue
 
         self.__count += where
 
@@ -89,7 +95,7 @@ class EmpiricalMean(BaseEstimator):
         updates multiple values
         """
         if type(oneHotIndexList) == type(None):
-            self.__count += newValue.shape[0]
+            self.__count += np.ones(self.__shape) * newValue.shape[0]
 
         else:
             self.__count += np.sum( oneHotIndexList, axis=0 )

barl/utils/numerical.py
@@ -30,14 +30,17 @@ def one_hot(listOfIndexes, numClasses):
     """
     idxes = np.array( listOfIndexes )
 
-    return np.eye( numClasses )[ idxes.reshape(-1) ]
+    temp = np.eye( numClasses )[ idxes.reshape(-1) ]
 
+    return np.copy(temp)
 
 def transform_to_one_hot(listOfIndexes, numClasses):
     """
     transforms a listOfIndexes into oneHot
     """
-    return np.squeeze( one_hot(listOfIndexes, numClasses) )
+    temp = np.squeeze( one_hot(listOfIndexes, numClasses) )
+
+    return np.copy(temp)
 
 
 
@@ -51,7 +54,9 @@ def transform_to_valued_one_hot(listOfValues, listOfIndexes, numClasses):
 
     vOneHots = oneHots * vals.reshape(-1,1)
 
-    return np.squeeze( vOneHots )
+    temp = np.squeeze( vOneHots )
+
+    return np.copy(temp)
 
 
 

tests/test_estimators.py
@@ -42,8 +42,8 @@ def test_empirical_mean_2():
     meanEst = EmpiricalMean([3])
 
     values = 2
-    idx = 0
-    out = np.array([2, 0, 0])
+    idx = 1
+    out = np.array([0, 2, 0])
 
     meanEst.update(values, idx)
 
@@ -74,6 +74,26 @@ def test_empirical_mean_4():
 
     assert np.array_equal( out, meanEst.get_estimate() ), "Empirical Mean 4"
 
+
+def test_empirical_mean_5():
+    meanEst = EmpiricalMean([3])
+
+    value1 = 1
+    value2 = 2
+    idx = 2
+
+    out = np.array([0,0,1.5])
+
+    meanEst.update(value1, idx)
+    meanEst.update(value1, idx)
+    meanEst.update(value1, idx)
+
+    meanEst.update(value2, idx)
+    meanEst.update(value2, idx)
+    meanEst.update(value2, idx)
+
+    assert np.array_equal( out, meanEst.get_estimate() ), "Empirical Mean 5"
+    
 ####################################################
 # MAIN
 ####################################################